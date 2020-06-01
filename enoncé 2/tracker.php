af39a125dd61e19288785140b4d26cab102c0f08
KhadijaZahir <57948334+KhadijaZahir@users.noreply.github.com>
2020-06-01 15:16:01 +0100

    enonce 2 and 3

--- enoncé 2/tracker.php
+++ enoncé 2/tracker.php
@@ -1,6 +1,25 @@
-<?php  
+<?php
 $name = $_GET["name"];
 $weightKg = $_GET["weightKg"];
-$heightCm = $_GET["heightCm"];
-$IBM = $weightKg/($heightCm**2);
+$heightCm = $_GET["heightCm"]; 
+
+// localhost:8085/tracker.php?name=khadijaZahir&weightKg=50&heightCm=160
+/* $name = 'andy';
+$weightKg = 50;
+$heightCm = 160; */
+$heightMeters = $heightCm/10;
+$heightSquared = $heightMeters * $heightMeters;
+
+$IMC = $weightKg/($heightSquared);
+if ($IMC < 18.5) 
+  {echo "<p>Hello $name, your IMC is $IMC you are underweight</p>";}
+
+if($IMC >= 18.5 and $IMC < 24.9)
+{echo "<p> Hello $name, your IMC is $IMC you are Healthy </p>";}
+
+elseif($IMC >= 24.9 and $IMC < 30)
+{echo "<p> Hello $name, your IMC is $IMC you have overweight </p>";}
+
+elseif($IMC >= 30)
+{echo"<p> Hello $name, your IMC is $IMC you have Suffering from Obesity </p>";}
 ?>
